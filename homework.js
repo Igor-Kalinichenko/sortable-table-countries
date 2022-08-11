2e5719ad97ef6adea0d579186f7f8e9d3b525188
Igor Kalinichenko <kaliv0601@gmail.com>
2022-08-11 12:40:31 +0300

    used arrow func, rest/spread operators

--- homework.js
+++ homework.js
@@ -2,11 +2,9 @@ let countries = [];
 let storedCountries = [];
 
 function checkSelect() {
-    document.querySelector('.countries-select').onchange = function(e) {
+    document.querySelector('.countries-select').onchange = e => {
         const value = e.currentTarget.value;
-        const filteredCountries = countries.filter(function(country) {
-            return country.region === value;
-        })
+        const filteredCountries = countries.filter(country => country.region === value)
         renderCountries(filteredCountries.length ? filteredCountries : countries);
         storedCountries = filteredCountries;
         document.getElementById('search').value = '';
@@ -14,16 +12,14 @@ function checkSelect() {
 }
 
 function renderSelect(countries) {
-    const uniqueRegions = countries.reduce(function(acc, country) {
+    const uniqueRegions = countries.reduce((acc, country) => {
         if(!acc.includes(country.region)) {
             acc.push(country.region);
         }
         return acc;
     }, []);
     let htmlStr = `<option value="">Not Selected</option>`;
-    htmlStr += uniqueRegions.map(function(region) {
-        return `<option value="${region}">${region}</option>`;
-    }).join('');
+    htmlStr += uniqueRegions.map(region => `<option value="${region}">${region}</option>`).join('');
 
     let selectElement = document.createElement('select');
     selectElement.className = "countries-select form-control my-3";
@@ -35,7 +31,7 @@ function renderSelect(countries) {
 
 function setListeners() {
     let tbody = document.querySelector('.table tbody');
-    tbody.onclick = function(e) {
+    tbody.onclick = e => {
         for(let item of document.querySelectorAll('table tbody td')) {
             item.classList.remove('bg-warning');
         }
@@ -45,17 +41,17 @@ function setListeners() {
 
 function sort() {
     for(let item of document.querySelectorAll('[data-attr]')) {
-        item.onclick = function(event) {
+        item.onclick = event => {
             
             for(let i of document.querySelectorAll('table thead th i')) {
                 i.parentNode.classList.remove('bg-warning');
-                i.className = '';
+                i.classList.add('d-none');
             }
 
             event.currentTarget.classList.add('bg-warning');
             let key = event.currentTarget.getAttribute('data-attr');
             let isSorted = event.currentTarget.getAttribute('data-sorted');
-            let sortedCountries = countries.sort(function(a, b) {
+            let sortedCountries = countries.sort((a, b) => {
                 if(isSorted){
                     return a[key] > b[key] ? -1 : 1;
                 }
@@ -64,10 +60,10 @@ function sort() {
 
             if(isSorted) {
                 event.currentTarget.removeAttribute('data-sorted');
-                event.currentTarget.lastChild.className = 'fa fa-arrow-down';
+                event.currentTarget.querySelector('.fa-arrow-down').classList.remove('d-none');
             } else {
                 event.currentTarget.setAttribute('data-sorted', '+');
-                event.currentTarget.lastChild.className = 'fa fa-arrow-up';
+                event.currentTarget.querySelector('.fa-arrow-up').classList.remove('d-none');
             }
             renderCountries(sortedCountries);
         }
@@ -75,13 +71,14 @@ function sort() {
 }
 
 function renderCountries(countries) {
-    const htmlStr = countries.reduce(function(acc, country, index) {
+    const htmlStr = countries.reduce((acc, country, index) => {
+        let {name, capital, area, region} = country;
         return acc + `<tr>
                     <td>${index + 1}</td>
-                    <td>${country.name}</td>
-                    <td>${country.capital || '---'}</td>
-                    <td>${country.region}</td>
-                    <td>${country.area}</td>
+                    <td>${name}</td>
+                    <td>${capital || '---'}</td>
+                    <td>${region}</td>
+                    <td>${area}</td>
                 </tr>`;
     }, '');
     document.querySelector('.table tbody').innerHTML = htmlStr;
@@ -89,35 +86,32 @@ function renderCountries(countries) {
     sort();
 }
 
-document.getElementById('search').onkeyup = function(e) {
+document.getElementById('search').onkeyup = e => {
     let searchValue = e.currentTarget.value.toLowerCase().trim();
     const filteredCountries = (storedCountries.length ? storedCountries : countries)
-        .filter(function(country) {
-        const name = country.name.toLowerCase();
-        const capital = country.capital.toLowerCase();
-        const region = country.region.toLowerCase();
-        return name.includes(searchValue)
-            || capital.includes(searchValue)
-            || region.includes(searchValue);
+        .filter(country => {
+            let {name, capital, region} = country;
+        return name.toLowerCase().includes(searchValue)
+            || capital.toLowerCase().includes(searchValue)
+            || region.toLowerCase().includes(searchValue);
     })
     renderCountries(filteredCountries);
 }
 
 
-document.querySelector('.google-link').onclick = function(e) {
+document.querySelector('.google-link').onclick = e => {
     let value = confirm('You are going to leave the page. Are you sure?');
     if(!value) {
         e.preventDefault();
     }
 }
 
-document.querySelector('.load-countries-btn').onclick = function() {
+document.querySelector('.load-countries-btn').onclick = () => {
     document.querySelector('.load-countries button').setAttribute('disabled', '');
-    fetch('https://restcountries.com/v2/all').then(function (data){
-        return data.json();
-    }).then(function(data) {
+    fetch('https://restcountries.com/v2/all').then(data => data.json()).then(data => {
+        console.log(data);
         document.querySelector('.load-countries button').removeAttribute('disabled');
-        countries = data.map(function(country) {
+        countries = data.map(country => {
             return {
                 name: country.name,
                 capital: country.capital || '',
@@ -131,5 +125,6 @@ document.querySelector('.load-countries-btn').onclick = function() {
         }
         renderCountries(countries);
         renderSelect(countries);
+
     })
 }
